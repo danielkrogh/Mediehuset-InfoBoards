5ca58e504ab3d3edccc2b3bde6c980faeed204b7
Daniel Krogh Andersen <daniel@kroghkommunikation.dk>
2020-11-02 10:24:16 +0100

    .

--- content/js/js.js
+++ content/js/js.js
@@ -15,8 +15,14 @@ function getActivitiesData() {
 }
 
 function sortActivitiesData() {
+    let classNameArr = [];
+
     return getActivitiesData().then((data) => {
-        return data;
+        data.result.forEach(element => {
+            classNameArr.push(element.class)
+        });
+
+        return classNameArr;
     })
 }
 
@@ -24,9 +30,31 @@ function sortActivitiesData() {
 /*
     View
 */
+const activitiesContainer = document.querySelector('#activities');
+
 function setActivities() {
-    sortActivitiesData().then((data) => {
-        console.log(data);
+    sortActivitiesData().then((classNameArr) => {
+        classNameArr.forEach(className => {
+            let p = document.createElement('p');
+
+            if (className.search('we') > 0) {
+                p.setAttribute('class', 'we')
+                p.innerHTML = 'Webudvikler'
+            } else if (className.search('gr') > 0) {
+                p.setAttribute('class', 'gr')
+                p.innerHTML = 'Grafisk Tekniker'
+            } else if (className.search('dm') > 0) {
+                p.setAttribute('class', 'dm')
+                p.innerHTML = 'Digital Medier'
+            } else if (className.search('mg') > 0) {
+                p.setAttribute('class', 'mg')
+                p.innerHTML = 'Mediegrafiker'
+            } else {
+                p.innerHTML = 'noget andet'
+            }
+
+            activitiesContainer.appendChild(p);
+        })
     })
 }
 
