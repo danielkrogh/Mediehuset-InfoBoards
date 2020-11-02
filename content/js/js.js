756073201ed3224ccd41aa4a2159a4e381db3183
Daniel Krogh Andersen <daniel@kroghkommunikation.dk>
2020-11-02 12:48:29 +0100

    .

--- content/js/js.js
+++ content/js/js.js
@@ -1,7 +1,7 @@
 /*
     Controller
 */
-function getActivitiesData() {
+function getActivitiesData() { // Henter data fra API
     return fetch('https://api.mediehuset.net/infoboard/activities')
         .then((resp) => {
             return resp.json()
@@ -14,7 +14,7 @@ function getActivitiesData() {
         })
 }
 
-function sortActivitiesData() {
+function sortActivitiesData() { // Behandler data fra API
     let activitiesArr = [];
 
     return getActivitiesData().then((data) => {
@@ -33,57 +33,65 @@ function sortActivitiesData() {
 const activitiesContainer = document.querySelector('#activities');
 let classShorthands = ['we', 'ggr', 'agr', 'abi', 'gr', 'dm', 'mg', 'iw'];
 
-function setActivities() {
+function setActivities() { // Opretter aktiviteter
     sortActivitiesData().then((activitiesArr) => {
         activitiesArr.forEach(activity => {
-            let p = document.createElement('p');
-
-            for (i = 0; i < classShorthands.length; i++) {
-                if (activity.class.search(`${classShorthands[i]}`) >= 0) {
-                    p.setAttribute('class', `${classShorthands[i]}`)
-                    p.innerHTML = `${changeName(classShorthands[i])} | ${activity.classroom} | ${activity.name} | ${activity.datetime}`;
-                    break;
-                } else {
-                    p.innerHTML = 'Ukendt uddannelse.'
-                }
+            if (new Date(activity.timestamp * 1000).getDate() == new Date().getDate() && (new Date().getHours() + 1) < 15) { // Hvis aktivitets dato er = dags dato og klokken er før kl. 15 vises dagens aktiviteter           
+                createActivities();
+            } else if (new Date(activity.timestamp * 1000).getDate() == (new Date().getDate() + 1) && (new Date().getHours() + 1) >= 15) { // Hvis aktivitets dato er i morgen og klokken er efter kl. 15 vises morgendagens aktiviteter
+                createActivities();
             }
 
-            activitiesContainer.appendChild(p);
+            function createActivities() {
+                let p = document.createElement('p');
 
-            function changeName(shorthand) {
-                let text;
-                switch (shorthand) {
-                    case 'we':
-                        text = 'Webudvikler';
-                        return text;
+                for (i = 0; i < classShorthands.length; i++) {
+                    if (activity.class.search(`${classShorthands[i]}`) >= 0) {
+                        p.setAttribute('class', `${classShorthands[i]}`)
+                        p.innerHTML = `${changeName(classShorthands[i])} | ${activity.classroom} | ${activity.name} | ${activity.datetime}`;
                         break;
-                    case 'ggr':
-                        text = 'Grafisk Tekniker Grundforløb 2';
-                        return text;
-                        break;
-                    case 'agr':
-                        text = 'AMU - Billedbehandling';
-                        return text;
-                        break;
-                    case 'abi':
-                        text = 'AMU - Grafisk';
-                        return text;
-                        break;
-                    case 'gr':
-                        text = 'Grafisk Tekniker';
-                        return text;
-                        break;
-                    case 'dm':
-                        text = 'Digital Medier';
-                        return text;
-                        break;
-                    case 'mg':
-                        text = 'Mediegrafiker';
-                        return text;
-                        break;
-                    case 'iw':
-                        text = 'It, web og medier';
-                        return text;
+                    } else {
+                        p.innerHTML = 'Ukendt uddannelse.'
+                    }
+                }
+
+                activitiesContainer.appendChild(p);
+
+                function changeName(shorthand) {
+                    let text;
+                    switch (shorthand) {
+                        case 'we':
+                            text = 'Webudvikler';
+                            return text;
+                            break;
+                        case 'ggr':
+                            text = 'Grafisk Tekniker Grundforløb 2';
+                            return text;
+                            break;
+                        case 'agr':
+                            text = 'AMU - Billedbehandling';
+                            return text;
+                            break;
+                        case 'abi':
+                            text = 'AMU - Grafisk';
+                            return text;
+                            break;
+                        case 'gr':
+                            text = 'Grafisk Tekniker';
+                            return text;
+                            break;
+                        case 'dm':
+                            text = 'Digital Medier';
+                            return text;
+                            break;
+                        case 'mg':
+                            text = 'Mediegrafiker';
+                            return text;
+                            break;
+                        case 'iw':
+                            text = 'It, web og medier';
+                            return text;
+                    }
                 }
             }
         })
