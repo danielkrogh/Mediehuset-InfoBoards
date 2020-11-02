ecfb66c8dab16301f3bafff78d3ab701822313d3
Daniel Krogh Andersen <daniel@kroghkommunikation.dk>
2020-11-02 10:38:13 +0100

    .

--- content/js/js.js
+++ content/js/js.js
@@ -37,18 +37,30 @@ function setActivities() {
         classNameArr.forEach(className => {
             let p = document.createElement('p');
 
-            if (className.search('we') > 0) {
+            if (className.search('we') >= 0) {
                 p.setAttribute('class', 'we')
                 p.innerHTML = 'Webudvikler'
-            } else if (className.search('gr') > 0) {
+            } else if (className.search('ggr') >= 0) {
+                p.setAttribute('class', 'gr')
+                p.innerHTML = 'Grafisk Tekniker Grundforløb 2'
+            } else if (className.search('agr') >= 0) {
+                p.setAttribute('class', 'amu')
+                p.innerHTML = 'AMU - Billedbehandling'
+            } else if (className.search('abi') >= 0) {
+                p.setAttribute('class', 'amu')
+                p.innerHTML = 'AMU - Grafisk'
+            } else if (className.search('gr') >= 0) {
                 p.setAttribute('class', 'gr')
                 p.innerHTML = 'Grafisk Tekniker'
-            } else if (className.search('dm') > 0) {
+            } else if (className.search('dm') >= 0) {
                 p.setAttribute('class', 'dm')
                 p.innerHTML = 'Digital Medier'
-            } else if (className.search('mg') > 0) {
+            } else if (className.search('mg') >= 0) {
                 p.setAttribute('class', 'mg')
                 p.innerHTML = 'Mediegrafiker'
+            } else if (className.search('iw') >= 0) {
+                p.setAttribute('class', 'iw')
+                p.innerHTML = 'It, web og medier'
             } else {
                 p.innerHTML = 'noget andet'
             }
